d2eafb6befbd1927e7f537f9c7f3ee2d5b9ab81d
kirtan0101 <kjpatel200022@gmail.com>
2024-12-13 21:50:11 +0530

    routes added.

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+
+import userRoutes from './routes/user.routers.js';
+
+app.use('/api/v1/users', userRoutes);
+
 export default app;
