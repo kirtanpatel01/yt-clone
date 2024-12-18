bded44bd9b8f78ae5bc90474905de7389759b0ca
kirtan0101 <kjpatel200022@gmail.com>
2024-12-19 00:19:07 +0530

    `Added new routes and functions to user routers`

--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/users.controllers.js";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateUserDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelDetails,
+    getUserWatchHistory,
+} from "../controllers/users.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 // import { verifyJWt } from "../middlewares/auth.middlewares.js";
@@ -7,19 +19,37 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(
-  upload.fields([
-    { name: "avatar", maxCount: 1, },
-    { name: "coverImage", maxCount: 1 },
-  ]),
-  registerUser
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        { name: "coverImage", maxCount: 1 },
+    ]),
+    registerUser,
 );
 
-router.route('/login').post(loginUser);
+router.route("/login").post(loginUser);
 
 // secured routes
-router.route('/logout').post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 
-router.route('/refresh-token').post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changePassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/update-account-details").patch(verifyJWT, updateUserDetails);
+
+router
+    .route("/update-avatar")
+    .post(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+router
+    .route("/update-coverImage")
+    .post(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route('/channel-details/:username').get(verifyJWT, getUserChannelDetails);
+
+router.route('/watch-history').get(verifyJWT, getUserWatchHistory);
 
 // router.route("/test-upload").post(
 //   upload.fields([
